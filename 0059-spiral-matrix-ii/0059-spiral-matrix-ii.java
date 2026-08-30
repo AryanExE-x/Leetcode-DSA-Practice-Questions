ba7c60372c0da1457bccd8d547502399a33b9361
Aryan Prasad <aryanprasad1405@gmail.com>
2026-08-30 16:48:35 +0530

    Time: 0 ms (100%), Space: 42.9 MB (62.55%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,29 +1,42 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int [][] arr = new int[n][n];
-        int firstRow=0;
-        int lastRow=n-1;
-        int firstCol=0;
-        int lastCol=n-1;
-        int val=1;
-        while(firstCol<=lastCol && firstRow<=lastRow){
-            for(int j=firstCol;j<=lastCol;j++){
-                arr[firstRow][j]=val++;
-            } //right
-            firstRow++;
-            for(int i=firstRow;i<=lastRow;i++){
-                arr[i][lastCol]=val++;
-            } //down
-            lastCol--;
-            for(int j=lastCol;j>=firstCol;j--){
-                arr[lastRow][j]=val++;
-            } //left
-            lastRow--;
-            for(int i=lastRow;i>=firstRow;i--){
-                arr[i][firstCol]=val++;
-            } //up
-            firstCol++;
+    int[][] arr = new int[n][n];
+
+    int firstRow = 0;
+    int lastRow = n - 1;
+    int firstCol = 0;
+    int lastCol = n - 1;
+    int val = 1;
+
+    while(firstCol <= lastCol && firstRow <= lastRow) {
+
+        for(int j = firstCol; j <= lastCol; j++) {
+            arr[firstRow][j] = val++;
         }
-        return arr;
+        firstRow++;
+
+        if(firstRow > lastRow || firstCol > lastCol) break;
+
+        for(int i = firstRow; i <= lastRow; i++) {
+            arr[i][lastCol] = val++;
+        }
+        lastCol--;
+
+        if(firstRow > lastRow || firstCol > lastCol) break;
+
+        for(int j = lastCol; j >= firstCol; j--) {
+            arr[lastRow][j] = val++;
+        }
+        lastRow--;
+
+        if(firstRow > lastRow || firstCol > lastCol) break;
+
+        for(int i = lastRow; i >= firstRow; i--) {
+            arr[i][firstCol] = val++;
+        }
+        firstCol++;
     }
+
+    return arr;
+}
 }
