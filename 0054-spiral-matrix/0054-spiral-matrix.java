44a4ab704adf30322e066259e53987ac45c2c866
Aryan Prasad <aryanprasad1405@gmail.com>
2026-08-30 16:00:08 +0530

    Time: 0 ms (100%), Space: 43 MB (43.77%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -3,23 +3,24 @@ class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
         int m = matrix.length; int n = matrix[0].length;
+        int tne=m*n;
         int firstRow=0; int lastRow=m-1; int firstCol=0; int lastCol=n-1;
-        while(firstRow<=lastRow && firstCol<=lastCol){
+        while(ans.size()<tne){
             for(int j=firstCol;j<=lastCol;j++){ //right
                 ans.add(matrix[firstRow][j]);
             }
             firstRow++;
-            if(firstRow>lastRow || firstCol>lastCol) break;
+            if(ans.size()==tne) break;
             for(int i=firstRow;i<=lastRow;i++){ //down
                 ans.add(matrix[i][lastCol]);
             }
             lastCol--;
-            if(firstRow>lastRow || firstCol>lastCol) break;
+            if(ans.size()==tne) break;
             for(int j=lastCol;j>=firstCol;j--){ //left
                 ans.add(matrix[lastRow][j]);
             }
             lastRow--;
-            if(firstRow>lastRow || firstCol>lastCol) break;
+            if(ans.size()==tne) break;
             for(int i=lastRow;i>=firstRow;i--){ //up
                 ans.add(matrix[i][firstCol]);
             }
